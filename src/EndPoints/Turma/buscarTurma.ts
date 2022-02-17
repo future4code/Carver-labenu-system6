26590ab27085096d7fdc9b05d515e790bde44f2d
Igor Matsuoka <igor.mtka@gmail.com>
2022-02-17 17:41:15 -0300

    atualizando buscarTurma

--- src/EndPoints/Turma/buscarTurma.ts
+++ src/EndPoints/Turma/buscarTurma.ts
@@ -3,9 +3,15 @@ import { connection } from "../../Data/connection";
 import { Turma } from "../../Classes/classeTurma";
 
 export const buscarTurma = async (req: Request, res: Response):Promise<void> => {
+    let errorCode = 500
     try {
         const turma: Turma[] = await connection("turma_6")
         .select()
+
+        if(turma.length === 0){
+            errorCode = 422
+            throw new Error("Não existem turmas ativas")
+        }
 
         res.status(201).send({turma: turma})
     } catch (error:any) {
