15e06089c913698579832ab8a242a57ff8c800d8
alisson <alissonabilio@gmail.com>
2022-02-18 17:44:25 -0300

    concluindo

--- src/EndPoints/Docente/createDocente.ts
+++ src/EndPoints/Docente/createDocente.ts
@@ -6,31 +6,77 @@ import { interfaceDocente, interfaceEspecialidade } from "../../Interfaces/inter
 export const createDocente = async (req: Request, res: Response): Promise<void> => {
     let errorCode = 500
     try {
-        const { nome, email, data_nasc, turma_id, } = req.body
+
+        const { nome, email, data_nasc,turma_id } = req.body
         const id = Date.now().toString()
-
         const especialidades: interfaceEspecialidade[] = req.body.especialidades
-        const especialidade = {
-            id: Date.now().toString(),
-            nome: especialidades
-        }
-        await connection("especialidade_6").insert(especialidade)
-
         const novoDocente = new Docente(id, nome, email, data_nasc, turma_id, especialidades)
+
 
         const docente: interfaceDocente = {
             id: novoDocente.getId(),
             nome: novoDocente.getName(),
             email: novoDocente.getEmail(),
             data_nasc: novoDocente.getDataNasc(),
-            turma_id: novoDocente.getTurmaId(),
+            turma_id:novoDocente.getTurmaId()
         }
 
         await connection("docente_6")
             .insert(docente)
 
-        res.status(201).send({ message: "Docente cadastrado com sucesso!" })
-    } catch (error: any) {
-        res.send({ error, message: error.message })
+            if (especialidades) {
+                for (let specialty of especialidades) {
+                    if (!specialty.nome){
+                        errorCode = 422;
+                        throw new Error("Each 'specialty' must received only a 'description' property! Please, check input´s values");
+                    } else if (
+                        specialty.nome !== "POO" &&
+                        specialty.nome !== "CSS" &&
+                        specialty.nome !== "JS" &&
+                        specialty.nome !== "React" &&
+                        specialty.nome !== "TYPESCRIPT"
+                    ) {
+                        errorCode = 422;
+                        throw new Error (`'Specialties' only received 'Backend', 'CSS', 'Programação Orientada a Objetos',
+                         'React', 'Redux', 'Testes' or 'Typescript' as value! Please, check input´s values`);
+                    };
+                };
+            };
+
+
+
+
+
+            // for (let especialidade of especialidades) {
+            //     let especialidadeId
+            //     const [existeEspecialidade] = await connection("especialidade_6")
+            //     .where("nome","like","%${especialidade}%")
+                
+               
+            //     if ([!existeEspecialidade]) {
+            //        errorCode = 422 
+            //        throw new Error("Inserir uma especialidade já predefinida.")
+            //     }else {
+            //         especialidadeId = existeEspecialidade.id
+            //     }
+                
+                
+            //     const docenteEspecialidade = {
+            //         id: Date.now().toString(),
+            //         docente_id: id,
+            //         especialidade_id: especialidadeId
+            //     }
+    
+            //     await connection("docente_especialidade_6").insert(docenteEspecialidade)
+            // }
+
+
+       
+
+        res.status(201).send({message: "Docente cadastrado com sucesso"})
+
+    } 
+    catch(error : any){
+        res.status(500).send(error.sqlmessage|| error.message)
     }
-}
+}    
